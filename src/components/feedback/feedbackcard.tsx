f84de91ed027174409ac93b3ef2abcef4187cce6
ramalingaa <87629304+ramalingaa@users.noreply.github.com>
2023-11-09 23:26:33 +0530

    feat: added time complexity

--- src/components/feedback/feedbackcard.tsx
+++ src/components/feedback/feedbackcard.tsx
@@ -23,19 +23,25 @@ const FeedbackCard = (props:any) => {
                       <div className="flex">
                           <div className = "border-right flex flex-col width-100">
                             <p className="font-semibold pb-2">Your Answer</p>
+                            
                             <Divider/>
                             <p className = "pt-2">{samples?.answer}</p>
+                            <p className = "pt-2">Time Complexity: {samples?.time_complexity}</p>
+                            <p className = "pt-2">Space Complexity: {samples?.space_complexity}</p>
                           </div>
                           <div className="flex flex-col pl-4 width-100">
                             <p className="font-semibold pb-2">Agent Feedback</p>
                             <Divider className = "divider-margin-negative"/>
                             <p className = "pt-2">{samples?.feedback}</p>
-                              {samples?.suggestedcode && <CodeBlock
+                            {samples?.suggestedcode &&
+                            <CodeBlock
                                 text={samples?.suggestedcode}
                                 language={"Javascript"}
                                 showLineNumbers={true}
                                 theme={dracula}
                               />}
+                            {samples?.explanation && <p className = "pt-2">Explanation: {samples?.explanation}</p>}
+
                           </div>
                       </div>
                       
